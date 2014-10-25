7a3ee811f2804fb31eaca90843538233c0b4f01f
Ben Conway <bendavidconway@me.com>
2014-10-25 17:14:35 +0100

    added and called methods

--- directory.rb
+++ directory.rb
@@ -24,13 +24,21 @@ students = [
 "Ben Conway"
 ]
 
-#then print 'em...
-students.each do |student|
-	puts student
+def print_header
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
 end
 
-#Finally, we print the total 
-puts "Overall, we have #{students.length} great students"
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
 
-
-
+def print_footer(names)
+  puts "Overall, we have #{names.length} great students"
+end 
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
